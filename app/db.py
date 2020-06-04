891912616cbd1f334f42e18f84fcd859b3924f49
tehhuu <volley_neverlose_exile@yahoo.co.jp>
2020-06-05 03:34:13 +0900

    minor correction

--- app/db.py
+++ app/db.py
@@ -27,10 +27,10 @@ session = SessionMaker()
 
 if __name__ == "__main__":
     #データベース作成処理。このファイルを直接実行すればデータベースを作成可能
-    user1 = User(name="Cho", password=hash("Cho"), email="bunshucho@gmail.com")
-    user2 = User(name="Takyu", password=hash("Takyu"), email="tkmz0216@gmail.com")
-    user3 = User(name="Kushino", password=hash("Kushino"), email="Kushino@gmail.com")
-    user4 = User(name="hait primary", password=hash("hait primary"), email="hait_primary_@gmail.com")
+    user1 = User(name="AAA", password=hash("AAA"), email="AAA@gmail.com")
+    user2 = User(name="BBB", password=hash("BBB"), email="BBB@gmail.com")
+    user3 = User(name="CCC", password=hash("CCC"), email="CCC@gmail.com")
+    user4 = User(name="DDD", password=hash("DDD"), email="DDD@gmail.com")
     session.add(user1)
     session.add(user2)
     session.add(user3)
